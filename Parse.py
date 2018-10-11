0d3d6567166e094c0dd0bad1f037263c34cb3567
Maria Firulyova <mariafirulevabio@gmail.com>
2018-10-12 00:39:28 +0300

    Remove some function due to its redundancy

--- Parse.py
+++ Parse.py
@@ -1,6 +1,6 @@
 def parse(bed_file):
 
-    def check(our_line: list):
+    def check_int(our_line: list) -> list:
         try:
             our_line[1] = int(our_line[1])
             our_line[2] = int(our_line[2])
@@ -11,5 +11,5 @@ def parse(bed_file):
     result = list(tuple())
     with open(bed_file, 'r') as file:
         for line in file:
-            result.append(check(line.split()[0:-1]))
+            result.append(check_int(line.split()[0:-1]))
     return result
