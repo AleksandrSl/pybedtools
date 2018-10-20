467cd1bf11b61b0c1d706cc9f34bb546ffe44fa1
Maria Firulyova <mariafirulevabio@gmail.com>
2018-10-20 10:39:20 +0300

    Changes has added

--- Parse.py
+++ Parse.py
@@ -1,6 +1,6 @@
 def parse(bed_file):
 
-    def check_int(our_line: list) -> list:
+    def check_int(our_line):
         try:
             our_line[1] = int(our_line[1])
             our_line[2] = int(our_line[2])
@@ -11,5 +11,5 @@ def parse(bed_file):
     result = list()
     with open(bed_file, 'r') as file:
         for line in file:
-            result.append(check_int(line.split()[0:-1]))
+            result.append(check_int(line.split()))
     return result
